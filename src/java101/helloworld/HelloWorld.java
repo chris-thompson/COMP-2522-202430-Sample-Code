7f32dac80c2d72a12eb3ea6a55bbdd7f577620a7
Chris Thompson <chrisgthompson@me.com>
2024-06-27 17:28:23 -0400

    Hello world welcome to the latest iteration of the BCIT DTC COMP 2522 sample code collection

---
+++ src/java101/helloworld/HelloWorld.java
@@ -0,0 +1,46 @@
+package java101.helloworld;
+
+/**
+ * HelloWorld.
+ *
+ * @author BCIT
+ * @version 2024
+ */
+public final class HelloWorld {
+
+    private HelloWorld() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+
+        final String message = "Hello world!";
+        System.out.println(message);
+
+        String emptyString = "";
+        System.out.println(emptyString);
+
+        String anotherString = new String("Long live the Oxford comma!");
+        System.out.println(anotherString);
+
+        String noString = null;
+        System.out.println(noString);
+        noString = "I have a value now";
+        System.out.println(noString);
+
+        final int theAnswerToEverything = 42;
+        // theAnswerToEverything = -1;
+        System.out.println(theAnswerToEverything);
+
+        final double goldenRatio = 1.6180339887498948482;
+        System.out.println(goldenRatio);
+
+        final int classSize = 5;
+        for (int i = 0; i < classSize; i++) {
+            System.out.println("Welcome");
+        }
+
+    }
+} // end of the class
